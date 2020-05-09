b233e9f030a44ba9891aa58ff8365567ca3adb3f
Thien An Ngo <>
2020-05-09 12:28:24 +0200

    no message

--- src3/import.py
+++ src3/import.py
@@ -10,7 +10,7 @@ db = scoped_session(sessionmaker(bind=engine))
 def main():
     f = open("flights.csv")
     reader = csv.reader(f)
-    for origin, destination, duration in reader:
+    for id, origin, destination, duration in reader:
         db.execute("INSERT INTO flights (origin, destination, duration) VALUES (:origin, :destination, :duration)",
                     {"origin": origin, "destination": destination, "duration": duration})
         print(f"Added flight from {origin} to {destination} lasting {duration} minutes.")
