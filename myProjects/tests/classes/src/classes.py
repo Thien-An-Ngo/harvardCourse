a6c4a6cebd5f2815c41806861933331395273528
Thien An Ngo <>
2020-05-28 10:08:26 +0200

    no message

--- myProjects/tests/classes/src/classes.py
+++ myProjects/tests/classes/src/classes.py
@@ -5,3 +5,6 @@ db = SQLAlchemy()
 class Awakened(db.Model):
     __tablename__ = "awakened"
     id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String, nullable=False)
+    last_name = db.Column(db.String, nullable=False)
+    
